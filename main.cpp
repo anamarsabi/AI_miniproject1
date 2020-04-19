09de926e15f7574279e4ffa4f2fa1be6ec5ce0be
IsaCarnoto <63849458+IsaCarnoto@users.noreply.github.com>
2020-04-19 16:19:45 +0200

    Add files via upload
    This is new 19/04/2020

--- main.cpp
+++ main.cpp
@@ -1,167 +1,375 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <cmath>
-#include <bits/stdc++.h>
+#include<iostream>
+#include<fstream>
+#include<queue>
+#include<vector>
+#include<string>
+#include<time.h>
 
 using namespace std;
-
-class Location{
-    public:
-        string name;
-        int x;
-        int y;
-        bool sorted;
-
-    Location(string aname, int ax, int ay, bool asorted){
-        name=aname;
-        x=ax;
-        y=ay;
-        sorted=asorted;
-    }
-
-    Location(){
-        name="x0";
+struct location{
+    double x; ///< Coordinate x
+    double y; ///< Coordinate y
+    int node;
+    bool sorted;
+	/**
+	 * @brief Constructor
+	 */
+    location(){
         x=0;
         y=0;
+        node=0;
         sorted=false;
+
+    }
+    location(double a,double b, int node_, bool sorted_) {
+        x=a;
+        y=b;
+        node=node_;
+        sorted=sorted_;
+    }
+    location(const location &a){
+        x=a.x;
+        y=a.y;
+        node=a.node;
+        sorted=a.sorted;
     }
 };
 
-vector<string> splitStrings(string str, char dl)
-{
-    //CODE FROM https://www.geeksforgeeks.org/split-string-substrings-using-delimiter/
-    string word = "";
+double d(location a, location b) {
+    return sqrt(pow(a.x-b.x,2)+pow(a.y-b.y,2));
+};
 
-    // to count the number of split strings
-    int num = 0;
 
-    // adding delimiter character at the end
-    // of 'str'
-    str = str + dl;
 
-    // length of 'str'
-    int l = str.size();
+double totalDistance(vector<location> &v) {
+    double sum = 0;
+    for(size_t i=0;i<v.size()-1;i++) {
+        sum += d(v[i],v[i+1]);
 
-    // traversing 'str' from left to right
-    vector<string> substr_list;
-    for (int i = 0; i < l; i++) {
+    }
+    return sum;
+}
 
-        // if str[i] is not equal to the delimiter
-        // character then accumulate it to 'word'
-        if (str[i] != dl)
-            word = word + str[i];
+queue<location> toQueue(vector<location> &v){
+    queue<location> r;
+    for(size_t k=0; k<v.size();k++){
 
-        else {
+        r.push(v[k]);
+    };
+    return r;
+}
 
-            // if 'word' is not an empty string,
-            // then add this 'word' to the array
-            // 'substr_list[]'
-            if ((int)word.size() != 0)
-                substr_list.push_back(word);
+//THIS FUNCTION TRANSFORMS FROM QUEUE TO VECTOR STARTING FROM INDEX 1
+vector<location> toVector(queue<location> &v){
+    vector<location> aux(v.size()+1);
+    int k=1;
+    while(!v.empty()){
+        aux[k]=v.front();
+        v.pop();
+        k++;
+    }
+    return aux;
+}
 
-            // reset 'word'
-            word = "";
+double minimum(double a, double b, double c, double d){
+    double mini=INT_MAX;
+    double aray[4]={a, b, c, d};
+
+    for(int i=0; i<4;i++){
+        if(aray[i]<mini){
+            mini=aray[i];
         }
     }
 
-    // return the splitted strings
-    return substr_list;
+    return mini;
 }
 
-double distance(Location l1, Location l2){
-    //if((l1=NULL) || (l2=NULL))
-       // return INT_MAX;
+	int main (){
 
-return sqrt(pow(l1.x-l2.x,2)+pow(l1.y-l2.y,2));
+    //We have to create an array of e (edges) with coordinates
+    struct location edges[13];
+    edges[0].x=0;
+    edges[0].y=0;
+    edges[0].node=0;
 
-};
+    int x0even=50;
+    int x0uneven=0;
+    int y0=-50;
+    int j=1;
 
-int main()
-{
+        for(int i=1;i<13;i++){
 
-    Location v0("v0", 0, 0, false);
-    Location e1("z1", 50, 0, false);
-    Location e2("z2", 150, 0, false);
-    Location e3("z3", 0, 50, false);
-    Location e4("z4", 100, 50, false);
-    Location e5("z5", 200, 50, false);
-    Location e6("z6", 50, 100, false);
-    Location e7("z7", 150, 100, false);
-    Location e8("z8", 0, 150, false);
-    Location e9("z9", 100, 150, false);
-    Location e10("z10", 200, 150, false);
-    Location e11("z11", 50, 200, false);
-    Location e12("z12", 150, 200, false);
+            if(j%2!=0){
+            edges[i].x=x0even;
+            edges[i].y=(j-1)*y0;
+            x0even=x0even+100;
+            }
 
-    string input;
+            if(j%2==0){
+            edges[i].x=x0uneven;
+            edges[i].y=(j-1)*y0;
+            x0uneven=x0uneven+100;
 
-    cout<< "Introduce a sequence of locations"<<endl;
-    cout<< "please use the format z3-z4-z7"<<endl;
+            }
 
-    getline(cin, input);
+            edges[i].node=i;
 
-    cout<<"Your entry is: "<<input<<endl;
-
-    vector<string> input_list = splitStrings(input, '-');
-
-    int totalLocations=12;
-
-    Location allLocations[totalLocations]={e1, e2, e3, e4, e5, e6, e7, e8, e9, e10 ,e11 ,e12};
-
-    Location deliveryList[input_list.size()];
-
-    for(int i=0; i<totalLocations;i++){
-
-        for(int j=0; j< input_list.size();j++){
-
-            if(allLocations[i].name==input_list[j])
-                deliveryList[j]=allLocations[i];
+            if((i==2)||(i==5)||(i==7)||(i==10)){
+                j++;
+                x0even=50;
+                x0uneven=0;
+            }
         };
+
+    for(int k=0; k<13;k++){
+
+        cout<<k<<": x: "<<edges[k].x<<", y: "<<edges[k].y<<endl;
     };
 
-    cout<<"delivery list"<<endl;
-    for(int k=0; k<input_list.size(); k++){
+    vector<location> dlist = {edges[6], edges[5], edges[12]};
+    cout<<"Delivery places"<<endl;
 
-        cout<<deliveryList[k].name<<endl;
+    for(size_t i=0; i<dlist.size();i++){
 
-    };
+    cout<<": x: "<<dlist[i].x<<", y: "<<dlist[i].y<<" node: "<<dlist[i].node<<endl;
 
+    }
+    cout<<endl;
     //SORTING
 
-    Location sortedList[input_list.size()+2];
-    sortedList[0]=v0;
+    //VECTOR TO STORE SORTED LIST
+    vector<location> slist(dlist.size()+2);
+
+    //DEFINING THE FIRST PLACE (PIZZERIA) AS 0,0
+    slist[0].x=0;
+    slist[0].y=0;
+    slist[0].node=0;
+
+    //SORTING THE VECTOR ACCORDING TO SMALLEST DISTANCES
+
     int pos;
     int dist;
     int mindist;
 
-    for(int j=0;j<input_list.size()+1;j++){
+    for(size_t j=0;j<dlist.size()+1;j++){
 
         mindist=INT_MAX;
         pos=0;
         dist=0;
 
-        for(int i=0; i<input_list.size();i++){
-            dist=distance(sortedList[j], deliveryList[i]);
+        for(size_t i=0; i<dlist.size();i++){
+            dist=d(slist[j], dlist[i]);
 
-            if((dist<=mindist)&&(dist!=0)&&(deliveryList[i].sorted==false)){
+            if((dist<=mindist)&&(dist!=0)&&(dlist[i].sorted==false)){
                 mindist=dist;
                 pos=i;
             }
         };
-        sortedList[j+1]=deliveryList[pos];
-        deliveryList[pos].sorted=true;
+        slist[j+1]=dlist[pos];
+        dlist[pos].sorted=true;
     }
 
-    sortedList[input_list.size()+1]=v0;
+    //ADDING THE LAS PLACE (PIZZERIA)
+    slist[slist.size()-1]=slist[0];
 
-    cout<<"sorted list"<<endl;
-    for(int k=0; k<input_list.size()+2; k++){
+    //SHOWING THE SORTED LIST
+    cout<<"Sorted places"<<endl;
+    for(size_t k=0; k<slist.size();k++){
 
-        cout<<sortedList[k].name<<endl;
+        cout<<k<<": x: "<<slist[k].x<<", y: "<<slist[k].y<<" node: "<<slist[k].node<<endl;
+    };
+    cout<<endl;
 
+    //TRANSFORMING THE VECTOR INTO A QUEUE
+    queue<location> squeue=toQueue(slist);
+
+
+    //MOVING
+
+    location temp=squeue.front();
+    //TEMPORARY MOVEMENTS IN EACH DIRECTION
+    location tempr;
+    location templ;
+    location tempu;
+    location tempd;
+    squeue.pop();//Eliminating the pizzeria because we are already there
+
+    //TEMPORARY VECTORS TO STORE THE DIFFERENT SORTED LISTS
+    vector<location> vec;
+    vector<location> vecr;
+    vector<location> vecl;
+    vector<location> vecu;
+    vector<location> vecd;
+
+    //QUEUE THAT GIVES THE FINAL ROUTE
+    queue<location> route;
+
+    //TEMPORARY QUEUES
+    queue<location> tq;
+    queue<location> aux=squeue;
+
+    //VALUE OF THE OBJECTIVE FUNCTIONS GOING IN DIFFERENT DIRECTIONS
+    double objr;
+    double objl;
+    double obju;
+    double objd;
+
+    //WEIGHT OF TRAVERSING A STREET
+    double w=100;
+
+    double num=1;
+    int index=0;
+    double mini;
+    bool brk=false;
+
+    //TRANSFORMING THE QUEUE BACK TO A VECTOR STARTING FROM [1]
+    vec=toVector(aux);
+
+    do{
+
+        //temporary location to the right
+        tempr.x=temp.x +50;
+        tempr.y=temp.y;
+
+        //temporary location to the left
+        templ.x=temp.x -50;
+        templ.y=temp.y;
+
+        //temporary location up
+        tempu.x=temp.x;
+        tempu.y=temp.y+50;
+
+        //temporary location down
+        tempd.x=temp.x;
+        tempd.y=temp.y-50;
+
+        //TEMPORARY VECTORS THAT EACH HAVE AS [0] THE TEMPORARY MOVEMENT AND THE REST
+        //IS THE REMAINING OF THE SORTED LIST
+        vecr=vec;
+        vecr[0]=tempr;
+
+        vecl=vec;
+        vecl[0]=templ;
+
+        vecu=vec;
+        vecu[0]=tempu;
+
+        vecd=vec;
+        vecd[0]=tempd;
+
+        //CALCULATING THE OBJECTIVE FUNCTIONS FOR EACH POSSIBLE VERCTOR
+        objr=w*num+totalDistance(vecr);
+        objl=w*num+totalDistance(vecl);
+        obju=w*num+totalDistance(vecu);
+        objd=w*num+totalDistance(vecd);
+
+        cout<<"Objective function going right: "<<objr<<endl;
+        cout<<"Objective function going left: "<<objl<<endl;
+        cout<<"Objective function going up: "<<obju<<endl;
+        cout<<"Objective function going down: "<<objd<<endl;
+
+    //CALCULATING THE WAY WITH THE MINIMUM OBJ. FUNCTION
+    mini=minimum(objr, objl, obju, objd);
+
+    //SEPARATING CASES OF MOVEMENT
+
+
+    if(mini==objr){//RIGHT IS THE SMALLEST
+
+        if((vecr[0].x==vecr[1].x)&&(vecr[0].y==vecr[1].y)){
+        //IF WE ARE ALREADY AT THE CORRESPONDING E WE POP IT OUT OF THE QUEUE
+        tq=toQueue(vecr);
+        tq.pop();
+        tq.pop();
+        squeue=tq;
+        vecr=toVector(tq);
+
+        }
+        //WE ADD TO THE VECTOR OUT CURRENT POSITION AS FIRST
+        temp.x=tempr.x+50;
+        temp.y=tempr.y;
+        vecr[0]=temp;
+        vec=vecr;
+
+    }
+    else if(mini==objl){ //LEFT IS THE SMALLEST
+        if((vecl[0].x==vecl[1].x)&&(vecl[0].y==vecl[1].y)){
+        //IF WE ARE ALREADY AT THE CORRESPONDING E WE POP IT OUT OF THE QUEUE
+        tq=toQueue(vecl);
+        tq.pop();
+        tq.pop();
+        squeue=tq;
+        vecl=toVector(tq);
+
+        }
+        //WE ADD TO THE VECTOR OUT CURRENT POSITION AS FIRST
+        temp.x=templ.x-50;
+        temp.y=templ.y;
+        vecl[0]=temp;
+        vec=vecl;
+    }
+    else if(mini==obju){//UP IS THE SMALLEST
+        if((vecu[0].x==vecu[1].x)&&(vecu[0].y==vecu[1].y)){
+        //IF WE ARE ALREADY AT THE CORRESPONDING E WE POP IT OUT OF THE QUEUE
+        tq=toQueue(vecu);
+        tq.pop();
+        tq.pop();
+        squeue=tq;
+        vecu=toVector(tq);
+
+        }
+        //WE ADD TO THE VECTOR OUT CURRENT POSITION AS FIRST
+        temp.x=tempu.x;
+        temp.y=tempu.y+50;
+        vecu[0]=temp;
+        vec=vecu;
+    }
+    else if(mini==objd){//DOWN IS THE SMALLEST
+        if((vecd[0].x==vecd[1].x)&&(vecd[0].y==vecd[1].y)){
+        //IF WE ARE ALREADY AT THE CORRESPONDING E WE POP IT OUT OF THE QUEUE
+        tq=toQueue(vecd);
+        tq.pop();
+        tq.pop();
+        squeue=tq;
+        vecd=toVector(tq);
+
+        }
+        //WE ADD TO THE VECTOR OUT CURRENT POSITION AS FIRST
+        temp.x=tempd.x;
+        temp.y=tempd.y-50;
+        vecd[0]=temp;
+        vec=vecd;
+    }
+
+
+    cout<<"List of current and future locations to visit"<<endl;
+    for(size_t k=0; k<vec.size();k++){
+
+        cout<<k<<": x: "<<vec[k].x<<", y: "<<vec[k].y<<endl;
     };
 
+    //ADDING OUT CURRENT POSITION TO THE FINAL ROUTE
+    route.push(vec[0]);
 
+    //BREAK CONDITION !!!!!!! DONT KNOW IF ITS ALWAYS RIGHT!!!!!
+    if((vec[0].x==0)&&(vec[0].y==0)&&(vec[1].x==0)&&(vec[1].y==0)){
+        brk=true;
+    }
+    index++;
+    num++;
+    }while(brk==false);
 
+    //PRINTING OUT THE FINAL ROUTE
+    cout<<"ROUTE "<<endl;
+    while(!route.empty()){
+
+        cout<<": x: "<<route.front().x<<", y: "<<route.front().y<<endl;
+        route.pop();
+    };
 
 
     return 0;
